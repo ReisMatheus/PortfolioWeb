8956de19c3ff2cb1617800cf3f5c4a22adf7d052
Matheus Vieira <Matheusmlcr@gmail.com>
2019-07-01 15:13:29 -0300

    add instagram feed

---
+++ backgroundCanvas.js
@@ -0,0 +1,8 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(canvas);
+
+var context = canvas.getContext('2d');
+var imgPath = '';
+
