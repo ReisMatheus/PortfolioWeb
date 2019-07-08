b34e225701d89ce1b15bfcbcf488ad7f3eb3ed99
Matheus Vieira <Matheusmlcr@gmail.com>
2019-07-08 17:49:09 -0300

    Add files via upload

--- interaction.js
+++ interaction.js
@@ -2,16 +2,13 @@
     var top = $('#mySidenav', '#optButton').offset().top - parseFloat($('#mySidenav', '#optButton').css('marginTop').replace(/auto/, 0));
     $(window).scroll(function (event) {
         var y = $(this).scrollTop();
-        //if y > top, it means that if we scroll down any more, parts of our element will be outside the viewport
-        //so we move the element down so that it remains in view.
         if (y >= top) {
            var difference = y - top;
            $('#mySidenav', '#optButton').css("top",difference);
        }
    });
 });
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
