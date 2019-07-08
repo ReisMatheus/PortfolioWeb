4e729fa743c21821dd6f0862f54f14bd4584a785
Matheus Vieira <Matheusmlcr@gmail.com>
2019-07-08 00:01:48 -0300

    Delete backgroundCanvas.js

--- backgroundCanvas.js
+++
@@ -1,8 +0,0 @@
-var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-console.log(canvas);
-
-var context = canvas.getContext('2d');
-var imgPath = '';
-
